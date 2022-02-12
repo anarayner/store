fbc9b3dfde691a11e62cfb1ec49c1f7c9b4cbee4
Victoria <95590803+anarayner@users.noreply.github.com>
2022-02-12 18:41:36 -0500

    dir

---
+++ client/src/pages/Basket.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const Basket = () => {
+    return (
+        <div>
+            <h1>BASKET</h1>
+
+        </div>
+    );
+};
+
+export default Basket;
---
+++ client/src/pages/DevicePage.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const DevicePage = () => {
+    return (
+        <div>
+            <h1>DEVICE</h1>
+
+        </div>
+    );
+};
+
+export default DevicePage;
